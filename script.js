7c80c9534cdf64c2f7a84ce64bba40768e768be3
Amy Snarr <amy.snarr16@gmail.com>
2023-06-01 10:15:14 -0600

    clicking on 'roll dice' works

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var diceArr = [];
 function initializeDice(){
 	for(i = 0; i < 6; i++){
 		diceArr[i] = {};
-		diceArr[i].id = "die" + i + 1;
+		diceArr[i].id = "die" + (i + 1);
 		diceArr[i].value = i + 1;
 		diceArr[i].clicked = 0;
 	}
@@ -23,20 +23,21 @@ function rollDice(){
 function updateDiceImg(){
 	var diceImage;
 	for(var i = 0; i < 6; i++){
-		diceImage = "images/" + i + ".png";
+		diceImage = "assets/" + diceArr[i].value + ".png";
 		document.getElementById(diceArr[i].id).setAttribute("src", diceImage);
 	}
 }
+
 
 function diceClick(img){
 	var i = img.getAttribute("data-number");
 
 	img.classList.toggle("transparent");
 	if(diceArr[i].clicked === 0){
-		diceArr[i].clicked == 1;
+		diceArr[i].clicked = 1;
 	}
 	else{
-		diceArr[i].clicked == 0;
+		diceArr[i].clicked = 0;
 	}
 
 }
